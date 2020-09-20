ec296460d89f0b8bb3e5030137a2b5b3c2cfe3a8
handa <31386906+handa9@users.noreply.github.com>
2020-09-20 22:44:01 +0900

    Support for LogUploader

--- dadget-server/src/server.js
+++ dadget-server/src/server.js
@@ -1,17 +1,25 @@
 import { ResourceNode } from '@chip-in/resource-node';
+import { LogUploader } from '@chip-in/logger'
 import Dadget from '@chip-in/dadget';
 
-if(!process.env.CORE_SERVER){
+if (!process.env.CORE_SERVER) {
   console.error("CORE_SERVER required");
   process.exit();
 }
-if(!process.env.RN_NAME){
+if (!process.env.RN_NAME) {
   console.error("RN_NAME required");
   process.exit();
 }
 
 let rnode = new ResourceNode(process.env.CORE_SERVER, process.env.RN_NAME);
-Dadget.getLogger().setLogLevel('info');
+let Logger = Dadget.getLogger();
+Logger.setLogLevel(process.env.LOG_LEVEL || "info");
+Logger.setMaxStringLength(process.env.LOG_MAX_LENGTH || 1024);
+if (process.env.USE_LOG_UPLOADER) {
+  LogUploader.registerServiceClasses(rnode);
+  Logger.attachUploader(rnode)
+}
+
 Dadget.registerServiceClasses(rnode);
 
 let jwtToken = process.env.ACCESS_TOKEN;
@@ -22,7 +30,7 @@ if (jwtToken) {
 
 rnode.start().then(() => {
   function sigHandle() {
-    rnode.stop().then(()=>{
+    rnode.stop().then(() => {
       process.exit()
     })
   }
